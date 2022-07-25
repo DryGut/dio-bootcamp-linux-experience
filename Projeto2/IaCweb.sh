6a8d507ce5254a12cbe8efe906a583fea509aa60
Renato Martins <91312588+DryGut@users.noreply.github.com>
2022-07-25 09:20:08 -0300

    projeto2 IaC atualizado

--- Projeto2/IaCweb.sh
+++ Projeto2/IaCweb.sh
@@ -4,8 +4,8 @@ echo "Atualizando o Sevidor..."
 
 apt-get update -y && apt-get upgrade -y
 
-apt-get install apache2
-apt-get install unzip
+apt-get install apache2 -y
+apt-get install unzip -y
 
 
 echo "Baixando e copiando os arquivos da Aplicação..."
@@ -15,4 +15,5 @@ wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.z
 cd /tmp/linux-site-dio-main
 cp -R * /var/www/html
 
+systemctl enable apache2
 systemctl start apache2
